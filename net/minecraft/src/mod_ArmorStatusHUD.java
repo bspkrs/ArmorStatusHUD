ca2b21367e312d2491697142c20f1364b6dd4822
bspkrs <bspkrs@gmail.com>
2012-11-13 16:44:41 -0500

    1.4.4 update

--- net/minecraft/src/mod_ArmorStatusHUD.java
+++ net/minecraft/src/mod_ArmorStatusHUD.java
@@ -54,7 +54,7 @@ public class mod_ArmorStatusHUD extends BaseMod
     
     private boolean checkUpdate;
     private ModVersionChecker versionChecker;
-    private String versionURL = "https://dl.dropbox.com/u/20748481/Minecraft/1.4.2/armorStatusHUD.version";
+    private String versionURL = "https://dl.dropbox.com/u/20748481/Minecraft/1.4.4/armorStatusHUD.version";
     private String mcfTopic = "http://www.minecraftforum.net/topic/1114612-";
     
     private static RenderItem itemRenderer = new RenderItem();
@@ -99,7 +99,7 @@ public class mod_ArmorStatusHUD extends BaseMod
     @Override
     public String getVersion() 
     {
-        return "v1.4(1.4.2)";
+        return "v1.4(1.4.4)";
     }
 
     @Override
