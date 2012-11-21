49c7a651fe8e88ebae8a2231f015adc33fbf85fd
bspkrs <bspkrs@gmail.com>
2012-11-21 02:13:24 -0500

    1.4.5 update

--- net/minecraft/src/mod_ArmorStatusHUD.java
+++ net/minecraft/src/mod_ArmorStatusHUD.java
@@ -1,83 +1,79 @@
 package net.minecraft.src;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Scanner;
 import java.util.logging.Level;
-import org.lwjgl.opengl.GL11;
 
-import bspkrs.util.client.HUDUtils;
-import bspkrs.util.client.ColorThreshold;
-import bspkrs.util.ModVersionChecker;
 import net.minecraft.client.Minecraft;
 
-public class mod_ArmorStatusHUD extends BaseMod 
+import org.lwjgl.opengl.GL11;
+
+import bspkrs.util.ModVersionChecker;
+import bspkrs.util.client.ColorThreshold;
+import bspkrs.util.client.HUDUtils;
+
+public class mod_ArmorStatusHUD extends BaseMod
 {
-    private static String defaultColorList = "100,f; 80,7; 60,e; 40,6; 25,c; 10,4";
+    private static String              defaultColorList     = "100,f; 80,7; 60,e; 40,6; 25,c; 10,4";
     
-    @MLProp(info="Set to true to allow checking for mod updates, false to disable")
-    public static boolean allowUpdateCheck = true;
-    @MLProp(info="Valid alignment strings are topleft, topcenter, topright, middleleft, middlecenter, middleright, bottomleft, bottomcenter, bottomright")
-    public static String alignMode = "bottomleft";
-    //@MLProp(info="Valid list mode strings are horizontal and vertical")
-    //public static String listMode = "vertical";
-    @MLProp(info="Set to true to show item names, false to disable")
-    public static boolean enableItemName = false;
-    @MLProp(info="Set to true to show the standard inventory item overlay (damage bar)")
-    public static boolean showItemOverlay = true;
-    @MLProp(info="This is a list of percent damage thresholds and text color codes that will be used when item damage is <= the threshold. Format used: \",\" separates the threshold and the color code, \";\" separates each pair. Valid color values are 0-9, a-f (color values can be found here: http://www.minecraftwiki.net/wiki/File:Colors.png)")
-    public static String damageColorList = defaultColorList;
-    @MLProp(info="Valid damageDisplayType strings are value, percent, or none")
-    public static String damageDisplayType = "value";
-    @MLProp(info="Set to true to show the max damage when damageDisplayType=value")
-    public static boolean showMaxDamage = false;
-    @MLProp(info="Set to true to show info for your currently equipped item, false to disable")
-    public static boolean showEquippedItem = true;
-    @MLProp(info="Horizontal offset from the edge of the screen (when using right alignments the x offset is relative to the right edge of the screen)")
-    public static int xOffset = 2;
-    @MLProp(info="Vertical offset from the edge of the screen (when using bottom alignments the y offset is relative to the bottom edge of the screen)")
-    public static int yOffset = 2;
-    @MLProp(info="Vertical offset used only for the bottomcenter alignment to avoid the vanilla HUD")
-    public static int yOffsetBottomCenter = 41;
-    @MLProp(info="Set to true if you want the xOffset value to be applied when using a center alignment")
-    public static boolean applyXOffsetToCenter = false;
-    @MLProp(info="Set to true if you want the yOffset value to be applied when using a middle alignment")
-    public static boolean applyYOffsetToMiddle = false;
-    @MLProp(info="Set to true to show info when chat is open, false to disable info when chat is open\n\n**ONLY EDIT WHAT IS BELOW THIS**")
-    public static boolean showInChat = false;
+    @MLProp(info = "Set to true to allow checking for mod updates, false to disable")
+    public static boolean              allowUpdateCheck     = true;
+    @MLProp(info = "Valid alignment strings are topleft, topcenter, topright, middleleft, middlecenter, middleright, bottomleft, bottomcenter, bottomright")
+    public static String               alignMode            = "bottomleft";
+    // @MLProp(info="Valid list mode strings are horizontal and vertical")
+    // public static String listMode = "vertical";
+    @MLProp(info = "Set to true to show item names, false to disable")
+    public static boolean              enableItemName       = false;
+    @MLProp(info = "Set to true to show the standard inventory item overlay (damage bar)")
+    public static boolean              showItemOverlay      = true;
+    @MLProp(info = "This is a list of percent damage thresholds and text color codes that will be used when item damage is <= the threshold. Format used: \",\" separates the threshold and the color code, \";\" separates each pair. Valid color values are 0-9, a-f (color values can be found here: http://www.minecraftwiki.net/wiki/File:Colors.png)")
+    public static String               damageColorList      = defaultColorList;
+    @MLProp(info = "Valid damageDisplayType strings are value, percent, or none")
+    public static String               damageDisplayType    = "value";
+    @MLProp(info = "Set to true to show the max damage when damageDisplayType=value")
+    public static boolean              showMaxDamage        = false;
+    @MLProp(info = "Set to true to show info for your currently equipped item, false to disable")
+    public static boolean              showEquippedItem     = true;
+    @MLProp(info = "Horizontal offset from the edge of the screen (when using right alignments the x offset is relative to the right edge of the screen)")
+    public static int                  xOffset              = 2;
+    @MLProp(info = "Vertical offset from the edge of the screen (when using bottom alignments the y offset is relative to the bottom edge of the screen)")
+    public static int                  yOffset              = 2;
+    @MLProp(info = "Vertical offset used only for the bottomcenter alignment to avoid the vanilla HUD")
+    public static int                  yOffsetBottomCenter  = 41;
+    @MLProp(info = "Set to true if you want the xOffset value to be applied when using a center alignment")
+    public static boolean              applyXOffsetToCenter = false;
+    @MLProp(info = "Set to true if you want the yOffset value to be applied when using a middle alignment")
+    public static boolean              applyYOffsetToMiddle = false;
+    @MLProp(info = "Set to true to show info when chat is open, false to disable info when chat is open\n\n**ONLY EDIT WHAT IS BELOW THIS**")
+    public static boolean              showInChat           = false;
     
-    private boolean checkUpdate;
-    private ModVersionChecker versionChecker;
-    private String versionURL = "https://dl.dropbox.com/u/20748481/Minecraft/1.4.4/armorStatusHUD.version";
-    private String mcfTopic = "http://www.minecraftforum.net/topic/1114612-";
+    private boolean                    checkUpdate;
+    private final ModVersionChecker    versionChecker;
+    private final String               versionURL           = "https://dl.dropbox.com/u/20748481/Minecraft/1.4.5/armorStatusHUD.version";
+    private final String               mcfTopic             = "http://www.minecraftforum.net/topic/1114612-";
     
-    private static RenderItem itemRenderer = new RenderItem();
-    protected float zLevel = 0.0F;
-    private ScaledResolution scaledResolution;
-    private List<ColorThreshold> colorList;
+    private static RenderItem          itemRenderer         = new RenderItem();
+    protected float                    zLevel               = 0.0F;
+    private ScaledResolution           scaledResolution;
+    private final List<ColorThreshold> colorList;
     
     public mod_ArmorStatusHUD()
-    {        
+    {
         colorList = new ArrayList<ColorThreshold>();
         try
         {
-            for(String s : damageColorList.split(";"))
+            for (String s : damageColorList.split(";"))
             {
                 String[] ct = s.split(",");
                 colorList.add(new ColorThreshold(Integer.valueOf(ct[0].trim()), ct[1]));
             }
         }
-        catch(Throwable e)
+        catch (Throwable e)
         {
             ModLoader.getLogger().log(Level.WARNING, "Error encountered parsing damageColorList: " + damageColorList);
             ModLoader.getLogger().log(Level.WARNING, "Reverting to defaultColorList: " + defaultColorList);
-            for(String s : defaultColorList.split(";"))
+            for (String s : defaultColorList.split(";"))
             {
                 String[] ct = s.split(",");
                 colorList.add(new ColorThreshold(Integer.valueOf(ct[0]), ct[1]));
@@ -85,25 +81,25 @@ public class mod_ArmorStatusHUD extends BaseMod
         }
         
         Collections.sort(colorList);
-
+        
         versionChecker = new ModVersionChecker(getName(), getVersion(), versionURL, mcfTopic, ModLoader.getLogger());
         checkUpdate = allowUpdateCheck;
     }
-
+    
     @Override
-    public String getName() 
+    public String getName()
     {
         return "ArmorStatusHUD";
     }
-
+    
     @Override
-    public String getVersion() 
+    public String getVersion()
     {
-        return "v1.4(1.4.4)";
+        return "v1.4(1.4.5)";
     }
-
+    
     @Override
-    public void load() 
+    public void load()
     {
         versionChecker.checkVersionWithLogging();
         ModLoader.setInGameHook(this, true, false);
@@ -112,41 +108,40 @@ public class mod_ArmorStatusHUD extends BaseMod
     @Override
     public boolean onTickInGame(float f, Minecraft mc)
     {
-        if((mc.inGameHasFocus || mc.currentScreen == null || (mc.currentScreen instanceof GuiChat && showInChat)) 
-                && !mc.gameSettings.showDebugInfo && !mc.gameSettings.keyBindPlayerList.pressed)
+        if ((mc.inGameHasFocus || mc.currentScreen == null || (mc.currentScreen instanceof GuiChat && showInChat)) && !mc.gameSettings.showDebugInfo && !mc.gameSettings.keyBindPlayerList.pressed)
         {
             scaledResolution = new ScaledResolution(mc.gameSettings, mc.displayWidth, mc.displayHeight);
             displayArmorStatus(mc);
         }
         
-        if(checkUpdate)
+        if (checkUpdate)
         {
-            if(!versionChecker.isCurrentVersion())
-                for(String msg : versionChecker.getInGameMessage())
+            if (!versionChecker.isCurrentVersion())
+                for (String msg : versionChecker.getInGameMessage())
                     mc.thePlayer.addChatMessage(msg);
             checkUpdate = false;
         }
         
         return true;
     }
-
+    
     private int getX(int width)
     {
-        if(alignMode.toLowerCase().contains("center"))
+        if (alignMode.toLowerCase().contains("center"))
             return scaledResolution.getScaledWidth() / 2 - width / 2 + (applyXOffsetToCenter ? xOffset : 0);
-        else if(alignMode.toLowerCase().contains("right"))
+        else if (alignMode.toLowerCase().contains("right"))
             return scaledResolution.getScaledWidth() - width - xOffset;
         else
             return xOffset;
     }
-
+    
     private int getY(int rowCount, int height)
     {
-        if(alignMode.toLowerCase().contains("middle"))
-            return (scaledResolution.getScaledHeight()/2) - ((rowCount * height)/2) + (applyYOffsetToMiddle ? yOffset : 0);
-        else if(alignMode.equalsIgnoreCase("bottomleft") || alignMode.equalsIgnoreCase("bottomright"))
+        if (alignMode.toLowerCase().contains("middle"))
+            return (scaledResolution.getScaledHeight() / 2) - ((rowCount * height) / 2) + (applyYOffsetToMiddle ? yOffset : 0);
+        else if (alignMode.equalsIgnoreCase("bottomleft") || alignMode.equalsIgnoreCase("bottomright"))
             return scaledResolution.getScaledHeight() - (rowCount * height) - yOffset;
-        else if(alignMode.equalsIgnoreCase("bottomcenter"))
+        else if (alignMode.equalsIgnoreCase("bottomcenter"))
             return scaledResolution.getScaledHeight() - (rowCount * height) - yOffsetBottomCenter;
         else
             return yOffset;
@@ -154,8 +149,7 @@ public class mod_ArmorStatusHUD extends BaseMod
     
     public static boolean playerHasArmorEquipped(EntityPlayer player)
     {
-        return player.inventory.armorItemInSlot(0) != null || player.inventory.armorItemInSlot(1) != null 
-                || player.inventory.armorItemInSlot(2) != null || player.inventory.armorItemInSlot(3) != null;
+        return player.inventory.armorItemInSlot(0) != null || player.inventory.armorItemInSlot(1) != null || player.inventory.armorItemInSlot(2) != null || player.inventory.armorItemInSlot(3) != null;
     }
     
     public static int countOfDisplayableItems(EntityPlayer player)
@@ -176,96 +170,97 @@ public class mod_ArmorStatusHUD extends BaseMod
     
     private void displayArmorStatus(Minecraft mc)
     {
-
+        
         if (playerHasArmorEquipped(mc.thePlayer) || (showEquippedItem && canDisplayItem(mc.thePlayer.getCurrentEquippedItem())))
         {
             int yOffset = enableItemName ? 18 : 16;
-
-            int yBase = getY(countOfDisplayableItems(mc.thePlayer),yOffset);
             
-            for(int i = 3; i >= -1; i--)
+            int yBase = getY(countOfDisplayableItems(mc.thePlayer), yOffset);
+            
+            for (int i = 3; i >= -1; i--)
             {
                 ItemStack item = null;
-                if(i == -1 && showEquippedItem)
+                if (i == -1 && showEquippedItem)
                     item = mc.thePlayer.getCurrentEquippedItem();
-                else if(i != -1)
+                else if (i != -1)
                     item = mc.thePlayer.inventory.armorInventory[i];
                 else
                     item = null;
-
+                
                 GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
                 
-                if(canDisplayItem(item))
+                if (canDisplayItem(item))
                 {
                     int xBase = 0;
                     int damage;
                     int maxDamage;
                     String itemDamage = "";
                     
-                    if(item.isItemStackDamageable())
+                    if (item.isItemStackDamageable())
                     {
                         maxDamage = item.getMaxDamage() + 1;
                         damage = maxDamage - item.getItemDamage();
-                        if(damageDisplayType.equalsIgnoreCase("value"))
-                            itemDamage = "\247" + ColorThreshold.getColorCode(colorList, damage * 100 / maxDamage) + damage 
-                                    + (showMaxDamage ? "\247f/" + maxDamage : "");
-                        else if(damageDisplayType.equalsIgnoreCase("percent"))
+                        if (damageDisplayType.equalsIgnoreCase("value"))
+                            itemDamage = "\247" + ColorThreshold.getColorCode(colorList, damage * 100 / maxDamage) + damage + (showMaxDamage ? "\247f/" + maxDamage : "");
+                        else if (damageDisplayType.equalsIgnoreCase("percent"))
                             itemDamage = "\247" + ColorThreshold.getColorCode(colorList, damage * 100 / maxDamage) + (damage * 100 / maxDamage) + "%";
                     }
-
-//                    if(item.itemID == Item.bow.shiftedIndex)
-//                        itemDamage = "(" + HUDUtils.countInInventory(mc.thePlayer, Item.arrow.shiftedIndex) + ") " + itemDamage;
+                    
+                    // if(item.itemID == Item.bow.shiftedIndex)
+                    // itemDamage = "(" +
+                    // HUDUtils.countInInventory(mc.thePlayer,
+                    // Item.arrow.shiftedIndex) + ") " + itemDamage;
                     
                     xBase = getX(18 + 4 + mc.fontRenderer.getStringWidth(HUDUtils.stripCtrl(itemDamage)));
                     
                     String itemName = "";
                     
-                    if(enableItemName)
+                    if (enableItemName)
                     {
                         itemName = StringTranslate.getInstance().translateNamedKey(item.getItem().getItemName());
                         xBase = getX(18 + 4 + mc.fontRenderer.getStringWidth(itemName));
                     }
                     
-                    //GL11.glEnable(GL11.GL_SCISSOR_TEST);
-                    //GL11.glPushMatrix();
-                    //GL11.glDisable(GL11.GL_BLEND);
+                    // GL11.glEnable(GL11.GL_SCISSOR_TEST);
+                    // GL11.glPushMatrix();
+                    // GL11.glDisable(GL11.GL_BLEND);
                     GL11.glEnable(32826 /* GL_RESCALE_NORMAL_EXT *//* GL_RESCALE_NORMAL_EXT */);
                     RenderHelper.enableStandardItemLighting();
                     RenderHelper.enableGUIStandardItemLighting();
                     itemRenderer.zLevel = 200.0F;
                     
-                    if(alignMode.toLowerCase().contains("right"))
+                    if (alignMode.toLowerCase().contains("right"))
                     {
                         xBase = getX(0);
                         this.itemRenderer.renderItemIntoGUI(mc.fontRenderer, mc.renderEngine, item, xBase - 18, yBase);
-                        if(showItemOverlay)
+                        if (showItemOverlay)
                             HUDUtils.renderItemOverlayIntoGUI(mc.fontRenderer, mc.renderEngine, item, xBase - 18, yBase);
                         
-                        RenderHelper.disableStandardItemLighting(); 
+                        RenderHelper.disableStandardItemLighting();
                         GL11.glDisable(32826 /* GL_RESCALE_NORMAL_EXT *//* GL_RESCALE_NORMAL_EXT */);
-
-                        //GL11.glPopMatrix();
-                        //GL11.glDisable(GL11.GL_SCISSOR_TEST);
-
+                        
+                        // GL11.glPopMatrix();
+                        // GL11.glDisable(GL11.GL_SCISSOR_TEST);
+                        
                         int stringWidth = mc.fontRenderer.getStringWidth(HUDUtils.stripCtrl(itemName));
                         mc.fontRenderer.drawStringWithShadow(itemName, xBase - 20 - stringWidth, yBase, 0xffffff);
                         stringWidth = mc.fontRenderer.getStringWidth(HUDUtils.stripCtrl(itemDamage));
-                        mc.fontRenderer.drawStringWithShadow(itemDamage, xBase - 20 - stringWidth, yBase + (enableItemName ? 9: 4), 0xffffff);
+                        mc.fontRenderer.drawStringWithShadow(itemDamage, xBase - 20 - stringWidth, yBase + (enableItemName ? 9 : 4), 0xffffff);
                     }
                     else
                     {
                         this.itemRenderer.renderItemIntoGUI(mc.fontRenderer, mc.renderEngine, item, xBase, yBase);
-                        if(showItemOverlay)
+                        if (showItemOverlay)
                             HUDUtils.renderItemOverlayIntoGUI(mc.fontRenderer, mc.renderEngine, item, xBase, yBase);
                         
                         RenderHelper.disableStandardItemLighting();
                         GL11.glDisable(32826 /* GL_RESCALE_NORMAL_EXT *//* GL_RESCALE_NORMAL_EXT */);
-
-                        //GL11.glPopMatrix();
-                        //GL11.glDisable(GL11.GL_SCISSOR_TEST);
+                        
+                        // GL11.glPopMatrix();
+                        // GL11.glDisable(GL11.GL_SCISSOR_TEST);
                         
                         mc.fontRenderer.drawStringWithShadow(itemName, xBase + 20, yBase, 0xffffff);
-                        mc.fontRenderer.drawStringWithShadow(itemDamage, xBase + 20, yBase + (enableItemName ? 9: 4), 0xffffff);
+                        mc.fontRenderer.drawStringWithShadow(itemDamage, xBase + 20, yBase + (enableItemName ? 9 : 4), 0xffffff);
                     }
                     
                     yBase += yOffset;
