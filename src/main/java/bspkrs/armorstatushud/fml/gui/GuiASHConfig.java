23da6dccd3836c7cba90b73e0b3bd620b05978e9
bspkrs <bspkrs@gmail.com>
2014-05-13 16:38:25 -0400

    fixed up config gui thing

--- src/main/java/bspkrs/armorstatushud/fml/gui/GuiASHConfig.java
+++ src/main/java/bspkrs/armorstatushud/fml/gui/GuiASHConfig.java
@@ -1,34 +1,16 @@
 package bspkrs.armorstatushud.fml.gui;
 
 import net.minecraft.client.gui.GuiScreen;
-import bspkrs.armorstatushud.ConfigElement;
 import bspkrs.armorstatushud.fml.Reference;
-import bspkrs.util.config.ConfigCategory;
 import bspkrs.util.config.ConfigProperty;
 import bspkrs.util.config.Configuration;
-import bspkrs.util.config.Property;
 import bspkrs.util.config.gui.GuiConfig;
-import bspkrs.util.config.gui.IConfigProperty;
 
 public class GuiASHConfig extends GuiConfig
 {
     public GuiASHConfig(GuiScreen parent) throws NoSuchMethodException, SecurityException
     {
-        super(parent, getProps(), true, "ArmorStatusHUD", true, GuiConfig.getAbridgedConfigPath(Reference.config.toString()));
-    }
-    
-    private static IConfigProperty[] getProps()
-    {
-        ConfigCategory cc = Reference.config.getCategory(Configuration.CATEGORY_GENERAL);
-        IConfigProperty[] props = new IConfigProperty[ConfigElement.values().length];
-        for (int i = 0; i < ConfigElement.values().length; i++)
-        {
-            ConfigElement ce = ConfigElement.values()[i];
-            Property prop = cc.get(ce.key());
-            if (prop != null)
-                props[i] = new ConfigProperty(prop, ce.propertyType());
-        }
-        
-        return props;
+        super(parent, (new ConfigProperty(Reference.config.getCategory(Configuration.CATEGORY_GENERAL))).getConfigPropertiesList(true),
+                true, Reference.MODID, true, GuiConfig.getAbridgedConfigPath(Reference.config.toString()));
     }
 }
